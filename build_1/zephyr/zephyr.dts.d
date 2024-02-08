f8eef24f90017858c191d6865e9a3d7f520704a4
MarlonHenq <marlon@marlonhenq.dev>
2024-02-08 08:49:07 -0300

    update minimal

---
+++ build_1/zephyr/zephyr.dts.d
@@ -0,0 +1,21 @@
+empty_file.o: /home/pop_os/ncs/v2.4.2/zephyr/misc/empty_file.c \
+ /home/pop_os/ncs/v2.4.2/zephyr/boards/arm/nrf52dk_nrf52810/nrf52dk_nrf52810.dts \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/arm/nordic/nrf52810_qfaa.dtsi \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/common/mem.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/arm/nordic/nrf52810.dtsi \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/arm/armv7-m.dtsi \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/common/skeleton.dtsi \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/sys/util_macro.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/sys/util_internal.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/sys/util_loops.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/common/freq.h \
+ /home/pop_os/ncs/v2.4.2/zephyr/dts/arm/nordic/override.dtsi \
+ /home/pop_os/ncs/v2.4.2/zephyr/boards/arm/nrf52dk_nrf52810/nrf52dk_nrf52810-pinctrl.dtsi
